05bb81dfba61afe9b720e37c0171522be73edbe4
duckyvisualcodes <141234883+duckyvisualcodes@users.noreply.github.com>
2023-08-02 12:14:15 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print ("Tournament Admin is online")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong")
+
+@bot.command()
+async def createteam(ctx):
+    def check(m):
+        if m.author == ctx.author and m.channel == ctx.channel:
+            try:
+                float(m.content)
+                return True
+            except ValueError
+                return False
+            return False
+    await ctx.send()
+    
+
+bot.run("MTEzNjAwMzUxMTU0NTQ5OTc3OQ.G1nGkj.tWdI1pGTvjocll0dQtLOgcP2xA_LvcHVAcAklA")
---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+from logging.config import dictConfig
+
+load_dotenv()
+
