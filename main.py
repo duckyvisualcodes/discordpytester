164e0a6a47b35d41679f06601b6ad0acc0f2edcf
mmau <mmau@itu.dk>
2023-08-05 13:20:25 +0200

    Changed setupcommands to be slash commands

--- main.py
+++ main.py
@@ -1,29 +1,62 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 import asyncio
 import gspread
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print ("Tournament Admin is online")
     await bot.add_cog(CreateTeams(bot))
     await bot.add_cog(CreateMatchday(bot))
     await bot.add_cog(MassMessage(bot))
+    synced = await bot.tree.sync()
+    print(f"Synced {len(synced)} command(s).")
+    print ("Tournament Admin is online")
 
-async def createTeamsAndCategory(ctx, *args, categoryName):
-        arguments = ', '.join(args)
-        guild = ctx.message.guild
-        category = discord.utils.get(guild.categories, name=categoryName.upper())
+async def createTeamsAndCategory(interaction: discord.Interaction, categoryName, args: str):
+        teams = list(args.split(","))
+        arguments = ""
+        guild = interaction.guild
+        category = discord.utils.get(guild.categories, name=categoryName)
         if not category:
             await guild.create_category(categoryName)
             category = discord.utils.get(guild.categories, name=categoryName)
-        for x in args:
-            channel = discord.utils.get(guild.text_channels, name=x.lower(), category=category)
+        for x in teams:
+            x=x.strip().replace(" ", "-")
+            channel = discord.utils.get(guild.text_channels, name=x, category=category)
             if not channel:
                 await guild.create_text_channel(x, category=category)
-        await ctx.send(f'{len(args)} Teams: {arguments}')   
+                arguments += x + ', '
+        if len(arguments)>0:
+            arguments = arguments.rstrip(', ') 
+            await interaction.response.send_message(f'{len(arguments.split(","))} team channels created: {arguments} in categeory {categoryName}')   
+        else:
+            await interaction.response.send_message(f'No new team channels created team channels already exists for: {args} in category {categoryName}')
+
+@bot.tree.command(name="hello")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message("Hello " + interaction.user.mention)
+
+@bot.tree.command(name="sync") 
+async def sync(ctx):
+    synced = await bot.tree.sync()
+    print(f"Synced {len(synced)} command(s).")
+
+async def SendMassMessageChosenChannels(ctx, message, *channel_ids):
+    for channel_id in channel_ids:
+            try:
+                channel = discord.utils.get(bot.get_all_channels(), name=channel_id)
+                if channel:
+                    await channel.send(message)
+                    await ctx.channel.send(f"Message sent successfully to channel {channel.mention}.")
+                else:
+                    await ctx.channel.send(f"Channel with ID {channel_id} not found.")
+            except discord.Forbidden:
+                await ctx.channel.send(f"I do not have permission to send messages in channel {channel_id}.")
+            except discord.HTTPException as e:
+                await ctx.channel.send(f"Failed to send the message to channel {channel_id}: {e}") 
 
 class MassMessage(commands.Cog, name= "Mass message commands"):
     def __init__(self, bot):
@@ -36,14 +69,13 @@ class MassMessage(commands.Cog, name= "Mass message commands"):
         raise NotImplementedError
 
     @commands.command(name="MassMessageAllChannels", help="Sends a chosen message in all channels")
-    async def MassMessageAllChannels(ctx, categoryName, *teams):
+    async def MassMessageAllChannels(self, ctx, categoryName, *teams):
         #TODO implement
         raise NotImplementedError  
     
-    @commands.command(name="MasMessageChosenChannels", help="Sends a chosen message in all chosen channel names")
-    async def MasMessageChosenChannels(ctx, categoryName, *teams):
-        #TODO implement
-        raise NotImplementedError  
+    @commands.command(name="MassMessageChosenChannels", help="Sends a chosen message in all chosen channel names")
+    async def MassMessageChosenChannels(self, ctx, message, *channel_name):
+        await SendMassMessageChosenChannels(ctx, message, *channel_name)
 
 
 class CreateTeams(commands.Cog, name= "Team Role & Channel Setup commands"):
@@ -51,15 +83,17 @@ class CreateTeams(commands.Cog, name= "Team Role & Channel Setup commands"):
         self.bot = bot
         self._last_member = None
 
-    @commands.command(name="setupTeamsWithoutCategory", help="Sets up team text channels under default category \'TEAMS\'" )
-    async def setupTeamsWithoutCategory(ctx, *teams ):
-        await createTeamsAndCategory(ctx, *teams, categoryName='teams')
+    @app_commands.command(name="setup-teams-without-category", description="Sets up team text channels under default category \'TEAMS\'" )
+    @app_commands.describe(teams = "What teams you want to create as comma seperated list")
+    async def setupteamswithoutcategory(self, interaction: discord.Interaction, teams: str):
+        await createTeamsAndCategory(interaction, 'teams', teams)
         #TODO Finish implementation
         raise NotImplementedError
 
-    @commands.command(name="setupTeamsWithCategory", help="Sets up team text channels with first argument being the category under which the channels go under")
-    async def setupTeamsWithCategory(ctx, categoryName, *teams):
-        await createTeamsAndCategory(ctx, *teams, categoryName=categoryName)   
+    @app_commands.command(name="setup-teams-with-category", description="Sets up team text channels with first argument being the category under which the channels go under")
+    @app_commands.describe(teams = "What teams you want to create as comma seperated list", categoryname = "What you want to name your custom category")
+    async def setupteamswithcategory(self, interaction: discord.Interaction, categoryname: str, teams: str):
+        await createTeamsAndCategory(interaction, categoryname, teams)   
         #TODO Finish implementation
         raise NotImplementedError 
 
@@ -78,59 +112,6 @@ class CreateMatchday(commands.Cog, name= "Matchday Channel Creation commands"):
     async def setupMatchdayChannelsWithCategory(ctx, categoryName, *teams):
         #TODO implement
         raise NotImplementedError
-
-
-class SupremeHelpCommand(commands.HelpCommand):
-    def get_command_signature(self, command):
-        return '%s%s %s' % (self.context.clean_prefix, command.qualified_name, command.signature)
-
-    async def send_bot_help(self, mapping):
-        embed = discord.Embed(title="Help", color=discord.Color.blurple())
-        for cog, commands in mapping.items():
-            filtered = await self.filter_commands(commands, sort=True)
-            if command_signatures := [
-                self.get_command_signature(c) for c in filtered
-            ]:
-                cog_name = getattr(cog, "qualified_name", "No Category")
-                embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
-
-        channel = self.get_destination()
-        await channel.send(embed=embed)
-
-    async def send_command_help(self, command):
-        embed = discord.Embed(title=self.get_command_signature(command) , color=discord.Color.blurple())
-        if command.help:
-            embed.description = command.help
-        if alias := command.aliases:
-            embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
-
-        channel = self.get_destination()
-        await channel.send(embed=embed)
-
-    async def send_help_embed(self, title, description, commands): # a helper function to add commands to an embed
-        embed = discord.Embed(title=title, description=description or "No help found...")
-
-        if filtered_commands := await self.filter_commands(commands):
-            for command in filtered_commands:
-                embed.add_field(name=self.get_command_signature(command), value=command.help or "No help found...")
-
-        await self.get_destination().send(embed=embed)
-
-    async def send_group_help(self, group):
-        title = self.get_command_signature(group)
-        await self.send_help_embed(title, group.help, group.commands)
-
-    async def send_cog_help(self, cog):
-        title = cog.qualified_name or "No"
-        await self.send_help_embed(f'{title} Category', cog.description, cog.get_commands())
-
-    async def send_error_message(self, error):
-        embed = discord.Embed(title="Error", description=error, color=discord.Color.red())
-        channel = self.get_destination()
-
-        await channel.send(embed=embed)
-
-bot.help_command = SupremeHelpCommand()
 
 bot.run("MTEzNjAwMzUxMTU0NTQ5OTc3OQ.G1nGkj.tWdI1pGTvjocll0dQtLOgcP2xA_LvcHVAcAklA")
 
