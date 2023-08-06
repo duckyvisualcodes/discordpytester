b6c11a63800a48e4e60bafef29af8e674c776a59
mmau <mmau@itu.dk>
2023-08-06 15:49:17 +0200

    made mass message slash commands

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from discord import CategoryChannel
 import asyncio
 import gspread
 
@@ -63,44 +64,54 @@ async def createTeamsAndCategory(interaction: discord.Interaction, categoryName:
 @bot.tree.command(name="hello")
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message("Hello " + interaction.user.mention)
-
-@bot.tree.command(name="sync") 
-async def sync(ctx):
-    synced = await bot.tree.sync()
-    print(f"Synced {len(synced)} command(s).")
-
-async def SendMassMessageChosenChannels(ctx, message, *channel_ids):
-    for channel_id in channel_ids:
+    
+async def SendMassMessageChosenChannels(interaction: discord.Interaction, message, channel_names):
+    channel_names_list = list(channel_names.lower().split(","))
+    response =""
+    for channel_name in channel_names_list:
+            channel_name=channel_name.strip().replace(" ", "-")
             try:
-                channel = discord.utils.get(bot.get_all_channels(), name=channel_id)
+                channel = discord.utils.get(bot.get_all_channels(), name=channel_name)
                 if channel:
                     await channel.send(message)
-                    await ctx.channel.send(f"Message sent successfully to channel {channel.mention}.")
+                    response += f"Message sent successfully to channel {channel.mention}.\n"
                 else:
-                    await ctx.channel.send(f"Channel with ID {channel_id} not found.")
+                   response += f"Channel with name {channel_name} not found.\n"
             except discord.Forbidden:
-                await ctx.channel.send(f"I do not have permission to send messages in channel {channel_id}.")
+                response += f"I do not have permission to send messages in channel {channel_name}.\n"
             except discord.HTTPException as e:
-                await ctx.channel.send(f"Failed to send the message to channel {channel_id}: {e}") 
+                response += f"Failed to send the message to channel {channel_name}: {e}\n"
+    await interaction.response.send_message(response) 
 
 class MassMessage(commands.Cog, name= "Mass message commands"):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None      
 
-    @commands.command(name="MassMessageChannelsInChosenCategory", help="Sends a chosen message in all channels in named category" )
-    async def MassMessageChannelsInChosenCategory(ctx, *teams ):
-        #TODO implement
-        raise NotImplementedError
+    @app_commands.command(name="message-channels-in-category", description="Sends a chosen message in all channels in named category")
+    @app_commands.describe(message = "The message you want to send to all channels in chosen category", category = "The category in which these should be send")
+    async def MassMessageChannelsInChosenCategory(self, interaction: discord.Interaction, message: str, category: CategoryChannel):
+        channels = category.text_channels
+        channels_str = ""
+        for channel in channels:
+            channels_str += channel.name + ', '
+        channels_str = channels_str.rstrip(', ')
+        await SendMassMessageChosenChannels(interaction, message, channels_str)
 
-    @commands.command(name="MassMessageAllChannels", help="Sends a chosen message in all channels")
-    async def MassMessageAllChannels(self, ctx, categoryName, *teams):
-        #TODO implement
-        raise NotImplementedError  
+    @app_commands.command(name="message-all-channels", description="Sends a chosen message in all channels")
+    @app_commands.describe(message = "The message you want to send to all channels")
+    async def MassMessageAllChannels(self, interaction: discord.Interaction, message: str):
+        channels = interaction.guild.text_channels
+        channels_str = ""
+        for channel in channels:
+            channels_str += channel.name + ', '
+        channels_str = channels_str.rstrip(', ')
+        await SendMassMessageChosenChannels(interaction, message, channels_str)  
     
-    @commands.command(name="MassMessageChosenChannels", help="Sends a chosen message in all chosen channel names")
-    async def MassMessageChosenChannels(self, ctx, message, *channel_name):
-        await SendMassMessageChosenChannels(ctx, message, *channel_name)
+    @app_commands.command(name="message-chosen-channels", description="Sends a chosen message in all chosen channel names")
+    @app_commands.describe(message = "The message you want to send to channels", channel_names = "A comma seperated list of channels you want to send the message to")
+    async def MessageChosenChannels(self, interaction: discord.Interaction, message: str, channel_names: str):
+        await SendMassMessageChosenChannels(interaction, message, channel_names)
 
 
 class CreateTeams(commands.Cog, name= "Team Role & Channel Setup commands"):
